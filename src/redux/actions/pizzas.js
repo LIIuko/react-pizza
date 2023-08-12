de109dce52c7716aade968ba5b761cd634c7c3ac
Kosty <doro.kon@mail.ru>
2023-08-12 14:34:08 +0300

    Json-sever

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -14,7 +14,7 @@ export const fetchPizzas = (category, sortBy) => (dispatch) => {
     dispatch(setLoaded(false));
     axios
         .get(
-            `pizzas?${category ? `category=${category - 1}&` : ''}_sort=${sortBy}&_order=asc`,
+            `https://my-json-server.typicode.com/LIIuko/react-pizza/pizzas?${category ? `category=${category - 1}&` : ''}_sort=${sortBy}&_order=asc`,
         )
         .then(({ data }) => {
             dispatch(setPizzas(data));
